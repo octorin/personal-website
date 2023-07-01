e451b7ba933960a2d6ad583a4a1f12b8aa2c5341
octorin <138330379+octorin@users.noreply.github.com>
2023-07-01 18:57:39 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 // next.config.js
-//from: https://www.taeny.dev/javascript/nextjs-with-deployment-platform#b4606b83f92e430bbcd4878ec6830a32
 const debug = process.env.NODE_ENV !== 'production'
 const name = 'small-magic-project-deployment'
 
